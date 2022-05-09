25eedc58d10496f967af0221f73e8ad212c27f94
Betta Splendid <forbiddenbetta@gmail.com>
2022-05-09 15:08:33 +0200

    Mail test work

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const bcrypt = require("bcrypt");
 
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'sarai.larkin37@ethereal.email',
+        pass: 'Uj8Nv97zeYGCPGSqnk'
+    }
+});
 
 // const mysql = require('mysql');
 // var con = mysql.createConnection({
@@ -137,6 +146,26 @@ app.post('/register', body("email").isEmail(), async(req, res) => {
     await prisma.user.create({
         data: data_to_send
     })
+
+    transporter.verify(function(error, success) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Server is ready to take our messages");
+        }
+    });
+
+    var info = await transporter.sendMail({
+        from: "sender@server.com",
+        to: data_to_send.email,
+        subject: "Wow you are so cool you registered.",
+        text: "Hell yeah bro",
+        html: "<p>HTML version of the message</p>"
+    });
+
+
+    console.log("Message sent: %s", info.messageId);
+
 })
 
 
